bf6509a7e5813a91137a6759205ab069f00ef138
Bonnie Zheng <bonniezheng@Bonnies-MacBook-Air-2.local>
2016-12-26 16:46:06 -0800

    This is my first commit

--- Module_1-02_intro/src/source.c
+++ Module_1-02_intro/src/source.c
@@ -2,18 +2,17 @@
 #include <math.h>
 #include "source.h"
 
-
-void three_lines(void)
-{
+void three_lines(void) {
+    /* Solution:
+     printf("January\nFebruary\nMarch\n");
+     */  
+    
     printf("January\n");
     printf("February\n");
     printf("March\n");
-    
 }
 
-
-void fix_types(void)
-{
+void fix_types(void) {
     float a1 = 2.1;
     float a2 = 3.2;
     float result_a = a1 + a2;
@@ -22,9 +21,11 @@ void fix_types(void)
     int b2 = 80000;
     int result_b = b1 * b2;
 
-    float c1 = 100000;
-    float c2 = 1.5;
+    int c1 = 100000;
+    double c2 = 1.5;
     int result_c = c1 / c2;
+
+
 
 
     // Do not change the following line
